a964b5964f4e80245fbc8c21cb12d450c98006dc
Steve Albonico <salbonico@gmail.com>
2019-02-01 18:32:24 +0000

    Automatically backed up by Learn

--- db/migrate/007_add_data_to_shows.rb
+++ db/migrate/007_add_data_to_shows.rb
@@ -1,10 +1,10 @@
-class AddDataToShows < ActiveRecord::Migration
+class AddDataToShows < ActiveRecord::Migration[5.1]
 
 def change
 
 add_column :shows, :day, :string
 add_column :shows, :season, :string
-add_column :shows, :genre, :string  
+add_column :shows, :genre, :string
 end
 
 
