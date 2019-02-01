a4501f124aaa0958bd242e15bc7cd53204c57fab
Steve Albonico <salbonico@gmail.com>
2019-02-01 17:40:43 +0000

    Automatically backed up by Learn

--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -4,6 +4,7 @@ class CreateCharacters < ActiveRecord::Migration[5.1]
     create_table :characters do |t|
       t.string :name
       t.integer :show_id
+      t.integer :actor_id
     end
   end
 end
